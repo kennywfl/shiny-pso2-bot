7360ab8a5ceafd9ce7a6da6869ce0f113f8783f6
Kennywfl <kennywfl@gmail.com>
2017-07-15 01:23:39 +0800

    Recommit missed file save

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
 
-client.login("MzI5OTI5OTgwMDc3MTQ2MTE0.DDZmww.9QTLSv4Lv9gtd19p_7ZEgL07JG4")
-
 client.on("ready", function() {
     console.log("I am ready!");
 });
@@ -29,3 +27,5 @@ client.on("message", function(message) {
         message.reply(data);
     }
 });
+
+client.login(config.token);
